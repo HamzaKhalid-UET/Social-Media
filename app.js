c1809a77b4beb46ae1a703b0e4899500075323ff
hamzakhalid-UET <hamzakhalid.654@gmail.com>
2025-05-05 16:07:10 +0500

    routes services and controller

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const { default: mongoose } = require("mongoose")
+const route = require("./routes/auth")
 const app = express()
 const Mongoose=require ("mongoose")
-
 const {MONGOURI} = require("./valuekeys")
 Mongoose.connect(MONGOURI)
 
+app.use("/",route)
 
 mongoose.connection.on("connected", () => {
     console.log("we are connected to mongo i.e Mongo DB")
